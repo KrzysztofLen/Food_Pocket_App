6f6cb4caa70c0f8b42d8a2f247a15f267c8bcc8c
Krzysiek <hakkai660@gmail.com>
2017-11-04 19:35:51 +0100

    add tabs logic

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,10 +1,81 @@
-const navElements = document.querySelectorAll('.pl-mobile-dictionary__menu li a');
+(function () {
+	'use strict';
 
-function addActive() {
-	for (let i = 0; i < navElements.length; i++) {
-		navElements[i].classList.remove('active');
-	}
-	this.classList.toggle('active');
-}
+	/**
+	 * The Tabs component.
+	 * @description
+	 */
 
-navElements.forEach(link => link.addEventListener('click', addActive));
+	const tabs = (options) => {
+		console.log(options);
+
+		const el = document.querySelector(options.el),
+			tabNavigationLinks = el.querySelectorAll(options.tabNavigationLinks),
+			tabContentContainers = el.querySelectorAll(options.tabContentContainers);
+		let activeIndex = 0;
+		let initCalled = false;
+
+		console.log(el);
+		console.log(tabNavigationLinks);
+		console.log(tabContentContainers);
+
+		/**
+		 * addActive
+		 * @description Add active class to the current tab.
+		 */
+
+		function addActive() {
+			for (let i = 0; i < tabNavigationLinks.length; i++) {
+				tabNavigationLinks[i].classList.remove('active');
+			}
+			this.classList.toggle('active');
+		}
+
+		tabNavigationLinks.forEach(link => link.addEventListener('click', addActive));
+
+
+		const init = () => {
+			if(!initCalled) {
+				initCalled = true;
+				el.classList.remove('no-js');
+
+				for(let i = 0; i < tabNavigationLinks.length; i++) {
+					const link = tabNavigationLinks[i];
+					handleClick(link, i);
+				}
+			}
+		};
+
+		const handleClick = (link, index) => {
+			link.addEventListener('click', (e) => {
+				e.preventDefault;
+				goToTab(index);
+			})
+		};
+
+		const goToTab = (index) => {
+			if(index !== activeIndex && index >= 0 && index <= tabNavigationLinks.length) {
+				tabContentContainers[activeIndex].classList.remove('is-active');
+				tabContentContainers[index].classList.add('is-active');
+				activeIndex = index;
+			}
+		};
+
+		return {
+			init: init,
+			goToTab: goToTab
+		};
+
+	};
+
+	window.tabs = tabs;
+
+})();
+
+const myTabs = tabs({
+	el: '#tabs',
+	tabNavigationLinks: '.pl-mobile-dictionary__nav__link',
+	tabContentContainers: '.c-tab'
+});
+
+myTabs.init();
