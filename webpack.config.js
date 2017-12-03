89f76ecc3956c30f4aa0676d2d96534ebfb6ef37
Krzysiek <hakkai660@gmail.com>
2017-12-03 12:05:00 +0100

    Added icon into form

--- webpack.config.js
+++ webpack.config.js
@@ -83,7 +83,7 @@ module.exports = {
 				}
 			},
 			{
-				test: /\.(eot|svg|ttf|otf|woff|woff2)$/,
+				test: /\.(eot|ttf|otf|woff|woff2)$/,
 				loader: 'file-loader',
 				options: {
 					name: 'fonts/[name].[ext]',
